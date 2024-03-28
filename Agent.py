015f4a67779337afc2a0d0817c51f0e7f7d40b41
beerhonkoop@gmail.com <G!7#ub66>
2024-03-28 22:42:38 +0100

    approximately correct structure

--- Agent.py
+++ Agent.py
@@ -12,12 +12,13 @@ import tensorflow
 #       the final train() function. I just added them as placeholders for the algorithm.
 
 class DQNAgent:
-    def __init__(self, learning_rate, exploration_factor, policy):
+    def __init__(self, learning_rate=0.001, exploration_factor=0.1, policy='egreedy', verbose=0):
         self.env = gym.make('CartPole-v1')
         self.learning_rate = learning_rate
         self.discount_factor = 0.99
         self.exploration_factor = exploration_factor
         self.policy = policy
+        self.verbose = verbose
 
         self.n_actions = 2
         self.max_steps_per_episode = 200
@@ -25,8 +26,8 @@ class DQNAgent:
 
         self.batch_size = 32  # Size of batch taken from replay buffer
 
-        self.Qnetwork = None
-        self.QTnetwork = None
+        self.Q_network = None
+        self.QT_network = None
         self.histories = []  # replay memory
         self.running_reward = 0
         self.episode_count = 0
@@ -39,8 +40,6 @@ class DQNAgent:
         reward_history = []
         done_history = []
         self.histories = [[action_history, state_history, next_state_history, reward_history, done_history]]
-        print(
-            f'self.histories = {self.histories},shape={np.asarray(self.histories).shape} type hist[0]={type(self.histories[0])}')
 
     def select_action(self, s):
         ''' Selects next action using initialized policy'''
@@ -49,17 +48,11 @@ class DQNAgent:
             epsilon = self.exploration_factor
 
             # extracting Q values from Qnetwork
-            print(f'state={s}')
-            state_tensor = keras.ops.convert_to_tensor(s)
-            print(f'state_tensor1={state_tensor},type={type(state_tensor)}')
-            state_tensor = keras.ops.expand_dims(state_tensor, 0)
-            print(f'state_tensor2={state_tensor},type={type(state_tensor)}')
-            action_probs = self.Qnetwork(state_tensor, training=False)
-            print(f'action_probs = {action_probs}')
+            s = np.expand_dims(s, axis=(0, 1))
+            action_probs = self.Q_network.predict(s, verbose=0)
 
             # epsilon greedy algorithm
             max_a = argmax(action_probs[0])
-            print(f'max_a = {max_a}')
             for a in range(self.n_actions):
                 if a == max_a:
                     probability = 1. - epsilon * (self.n_actions - 1) / self.n_actions
@@ -67,87 +60,101 @@ class DQNAgent:
                     probability = epsilon / self.n_actions
                 probabilities.append(probability)
             chosen_a = choice(range(self.n_actions), 1, p=probabilities)
-            print(f'chosen_a = {chosen_a}')
-            return int(chosen_a)
 
+            if self.verbose:
+                print(f'    state={s}')
+                print(f'    action_probs = {action_probs}')
+                print(f'    max_a = {max_a}, chosen_a = {chosen_a}')
+            return int(chosen_a)
         if self.policy == 'softmax':
             return
-
-        else:
-            print('please specify a policy')
+        else: print('please specify a policy:{egreedy, softmax}')
 
     def execute_action(self, a):
         observation, reward, terminated, truncated, info = self.env.step(a)
         return observation, reward, terminated, truncated
 
-    def sample_from_replay_memory(self):
+    def sample_batch(self, ):
+        '''Samples a batch of size {self.batch_size}, from replay history/memory '''
         if len(self.histories) > self.batch_size:
-            sample = np.random.choice(self.histories, size=self.batch_size)
+            index_samples = np.random.choice(len(self.histories), size=self.batch_size, replace=False)
+            sample = [self.histories[i] for i in index_samples]
         else:
-            sample = self.histories
-        print(f'sample length= {len(sample)}')
-        print(f'first entry={sample[0]}')
+            sample = self.histories[1:]
+        if self.verbose: print(f'    sample length= {len(sample)},   first batch item ={sample[0]}')
         return sample
 
     def update_networks(self, i, batch):
-        target_update_interval = 50
-
+        # not 100% confident i have this set up correctly
+        # could be a mixup in QT_network and Q_network in network.predict(s)
+        target_update_interval = 25 #50
         for action, state, next_state, reward, done in batch:
+            if i > 30:
+                print(f'{i}: {np.shape(batch)}'
+                      f'next state size = {np.shape(next_state)}: {next_state}'
+                      f'{batch}')
+            next_state = np.expand_dims(next_state, axis=(0, 1))
+            state = np.expand_dims(state, axis=(0, 1))
             G = reward
             if not done:
-                G = reward + self.discount_factor * np.amax(self.QTnetwork.predict(next_state)[0])
+                target_predictions = self.QT_network.predict(next_state, verbose=0)
+                G = reward + self.discount_factor * np.amax(target_predictions[0])
 
-            prediction = self.QTnetwork.predict(state)
+            prediction = self.QT_network.predict(state, verbose=0)
             prediction[0][action] = G
-            self.Qnetwork.fit(state, prediction, epochs=1, verbose=0)
+            self.Q_network.fit(state, prediction, epochs=1, verbose=0)
         if i % target_update_interval == 0:
-            self.QTnetwork.set_weights(self.Qnetwork.get_weights())
+            self.QT_network.set_weights(self.Q_network.get_weights())
 
     def evaluate(self, eval_env, n_eval_episodes, max_episode_length=200):
         ''' Evaluates currently learned strategy '''
         return
 
-    def train(self):
-        learning_rate = 0.9
-        exploration_factor = 1
-        policy = 'egreedy'
-
-        # initialize environment, replay buffer, network & target network
-        observation, info = self.env.reset()
-        state = np.array(observation)
-
-        self.initialize_replay_memory()
-        self.Qnetwork = DeepNeuralNetwork.custom_Qnetwork
-        self.QTnetwork = DeepNeuralNetwork.custom_Qnetwork
+    def train(self, nr_layers=1, neurons_per_layer=16):
+        # initialize neural network & target network
+        DNN = DeepNeuralNetwork(nr_layers=nr_layers, neurons_per_layer=neurons_per_layer,
+                                learning_rate=self.learning_rate)
+        self.Q_network = DNN.custom_Qnetwork()
+        self.QT_network = DNN.custom_Qnetwork()
 
         # initial values
-        episode_reward = 0
+        for i in range(self.max_episodes):
+            # initialize environment & replay buffer
+            self.initialize_replay_memory()
+            observation, info = self.env.reset()
+            state = np.array(observation)
 
-        for i in range(5):
-            # select next action
-            action = self.select_action(state)
+            episode_reward = 0
+            for j in range(self.max_steps_per_episode):
+                if self.verbose: print(f'\nepisode {i}: iteration {j}')
+                # select next action
+                action = self.select_action(state)
 
-            # evaluate next state & reward
-            next_state, reward, done, trunc = self.execute_action(action)
-            episode_reward += reward
+                # evaluate next state & reward
+                next_state, reward, done, trunc = self.execute_action(action)
+                episode_reward += reward
 
-            # update replay buffer
-            print(f'appending {[action, state, next_state, reward, done]}')
-            print(type(action))
-            print(type(state))
-            print(type(next_state))
-            print(type(reward))
-            print(type(done))
-            self.histories.append([action, state, next_state, reward, done])
-            state = next_state
+                # update replay buffer
+                self.histories.append([action, state, next_state, reward, done])
+                # action ->   <class 'int'>
+                # state ->    <class 'numpy.ndarray'> size (4,)
+                # next_state> <class 'numpy.ndarray'> size (4,)
+                # reward ->   <class 'float'>
+                # done ->     <class 'bool'>
+                state = next_state
 
-            # sample a batch from replay buffer (once there are enough entries for batch)
-            batch = self.sample_from_replay_memory()
+                # sample a batch from replay buffer (once there are enough entries for batch)
+                batch = self.sample_batch()
 
-            # use batch to train Qnetwork, trains QTnetwork every 50 iterations
-            self.update_networks(i, batch)
+                # use batch to train Qnetwork, trains QT_network every 50 iterations
+                self.update_networks(j, batch)
+
+                if done:
+                    print(f'episode {i} terminated at iteration {j} - episode reward = {episode_reward}')
+                    break
         self.env.close()
         return
 
 
-DQNAgent.train()
+agent = DQNAgent(verbose=0)
+agent.train()
--- Neural_network.py
+++ Neural_network.py
@@ -3,7 +3,7 @@ from keras import layers,optimizers,losses
 
 
 class DeepNeuralNetwork:
-    def __init__(self, nr_layers=1, neurons_per_layer=16):
+    def __init__(self, nr_layers=1, neurons_per_layer=16,learning_rate=0.001):
         # super(DeepNeuralNetwork, self).__init__()
         '''Dense Neural Network
         params
@@ -12,16 +12,18 @@ class DeepNeuralNetwork:
         '''
         self.nr_layers = nr_layers
         self.neurons_per_layer = neurons_per_layer
+        self.learning_rate = learning_rate
         self.input_size = (1, 4)
         self.num_actions = 2
 
     def custom_Qnetwork(self, verbose=False):
         network = keras.Sequential()
-        network.add(keras.layers.Flatten(input_shape=self.input_size))
+        network.add(keras.Input(shape=self.input_size))
+        network.add(keras.layers.Flatten())
         for i in range(self.nr_layers):
             network.add(keras.layers.Dense(self.neurons_per_layer, activation='relu'))
         network.add(keras.layers.Dense(self.num_actions, activation='linear'))
         if verbose:
             print(network.summary())
-        network.compile(optimizer=keras.optimizers.adam_v2,loss=keras.losses.binary_crossentropy)
+        network.compile(optimizer=keras.optimizers.Adam(learning_rate=self.learning_rate),loss=keras.losses.binary_crossentropy)
         return network
