4058bc099185e389dcf56aebb2b5474e7b3d7bda
B.Kreft <bartosz.kreft.biz@gmail.com>
2024-03-31 23:20:57 +0200

    I think it works

--- Agent.py
+++ Agent.py
@@ -139,3 +139,111 @@ class DQNAgent:
         print('Steps: ', steps)
         return loss_avg, steps 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------ Chats code -----------
+# import random
+# from collections import deque
+# import numpy as np
+# import gymnasium as gym
+# from tensorflow.keras import models, layers, optimizers
+
+# class DQNAgent:
+#     def __init__(self, state_size, action_size):
+#         self.state_size = state_size
+#         self.action_size = action_size
+#         self.memory = deque(maxlen=2000)
+#         self.gamma = 0.95    # discount rate
+#         self.epsilon = 1.0   # exploration rate
+#         self.epsilon_min = 0.01
+#         self.epsilon_decay = 0.995
+#         self.learning_rate = 0.001
+#         self.model = self._build_model()
+#         self.target_model = self._build_model()
+#         self.update_target_model()
+
+#     def _build_model(self):
+#         # Neural Net for Deep-Q learning Model
+#         model = models.Sequential()
+#         model.add(layers.Dense(24, input_dim=self.state_size, activation='relu'))
+#         model.add(layers.Dense(24, activation='relu'))
+#         model.add(layers.Dense(self.action_size, activation='linear'))
+#         model.compile(loss='mse', optimizer=optimizers.Adam(learning_rate=self.learning_rate))
+#         return model
+
+#     def update_target_model(self):
+#         # Copy weights from model to target_model
+#         self.target_model.set_weights(self.model.get_weights())
+
+#     def remember(self, state, action, reward, next_state, done):
+#         self.memory.append((state, action, reward, next_state, done))
+
+#     def act(self, state):
+#         if np.random.rand() <= self.epsilon:
+#             return random.randrange(self.action_size)
+#         act_values = self.model.predict(state)
+#         return np.argmax(act_values[0])  # returns action
+
+#     def replay(self, batch_size):
+#         minibatch = random.sample(self.memory, batch_size)
+#         for state, action, reward, next_state, done in minibatch:
+#             target = reward
+#             if not done:
+#                 target = (reward + self.gamma * np.amax(self.target_model.predict(next_state)[0]))
+#             target_f = self.model.predict(state)
+#             target_f[0][action] = target
+#             self.model.fit(state, target_f, epochs=1, verbose=0)
+#         if self.epsilon > self.epsilon_min:
+#             self.epsilon *= self.epsilon_decay
+
+#     def load(self, name):
+#         self.model.load_weights(name)
+
+#     def save(self, name):
+#         self.model.save_weights(name)
+
+#     def run(self):
+#         env = gym.make('CartPole-v1')
+#         scores = deque(maxlen=100)
+#         for e in range(10000):  # You may want to choose another criterion for stopping
+#             state = env.reset()
+#             state = np.reshape(state, [1, self.state_size])
+#             for time in range(500):  # CartPole-v1 enforced max step
+#                 action = self.act(state)
+#                 next_state, reward, done, _ = env.step(action)
+#                 next_state = np.reshape(next_state, [1, self.state_size])
+#                 self.remember(state, action, reward, next_state, done)
+#                 state = next_state
+#                 if done:
+#                     print(f"episode: {e}/{10000}, score: {time}, e: {self.epsilon:.2}")
+#                     break
+#             scores.append(time)
+#             if len(scores) == 100 and np.mean(scores) >= 195.0:
+#                 print(f"Solved after {e} episodes!")
+#                 break
+#             if len(self.memory) > batch_size:
+#                 self.replay(batch_size)
+#             self.update_target_model()
+
+#         env.close()
+
+# # Hyperparameters
+# state_size = 4  # This is for CartPole-v1, adjust according to your environment
+# action_size = 2  # This is for CartPole-v1, adjust according to your environment
+# batch_size = 32  # Typically chosen between 32 and 64
+
+# agent = DQNAgent(state_size, action_size)
+# agent.run()
